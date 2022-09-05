f290169b263f456d1b2919881ffa9dd588f8e616
Fátima Montano <72367655+fatimaMontano@users.noreply.github.com>
2022-09-05 10:17:37 -0600

    Add files via upload

---
+++ ejercicio1/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      <Form></Form>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
